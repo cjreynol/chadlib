2179dd6f0befd8ccb61d96246181cdb6e17753c5
Chad Reynolds <cjreynol13@aol.com>
2019-04-21 16:05:47 -0500

    Rewrite TextEntryDialog using the new Dialog
    The Dialog class is a base class that encompasses most of the logic
    for a simple dialog class that returns one result to the calling
    application.

---
+++ chadlib/gui/dialog.py
@@ -0,0 +1,78 @@
+from abc                import ABC, abstractmethod
+from tkinter            import Button, Entry, Toplevel
+
+from chadlib.utility    import event_wrapper
+
+
+class Dialog(Toplevel, ABC):
+    """
+    Generic widget to capture dialogs which follow the pattern of allowing 
+    the user to provide some input and then passing that input to some 
+    callback.
+    """
+
+    def __init__(self, title_text, callback, callback_args, validation_check):
+        super().__init__()
+
+        self.title(title_text)
+        self._callback = callback
+        self._callback_args = callback_args
+        self._validation_check = validation_check
+
+        self._create_widgets()
+        self._arrange_widgets()
+        self._bind_actions()
+
+    def _create_widgets(self):
+        """
+        Create the widgets for the dialog.
+        """
+        self.confirm_button = Button(self, text = "Confirm")
+        self.cancel_button = Button(self, text = "Cancel")
+
+    @abstractmethod
+    def _arrange_widgets(self):
+        """
+        Place the widgets on the dialog window.
+        """
+        pass
+
+    def _bind_actions(self):
+        """
+        Bind methods to the buttons and keybindings.
+        """
+        self.confirm_button["command"] = self._confirm
+        self.bind("<Return>", event_wrapper(self._confirm))
+
+        self.cancel_button["command"] = self._cancel
+        self.bind("<Escape>", event_wrapper(self._cancel))
+
+    @abstractmethod
+    def _get_data(self):
+        """
+        Get the data from the dialog to pass to the callback.
+        """
+        pass
+
+    def _confirm(self):
+        """
+        Call the given callback method with the given data and data from the 
+        dialog, then close itself.
+
+        If a validation check was given, ensure it passed before passing the 
+        data back.
+        TODO CJR:  Feedback if the validation check fails.
+        """
+        data = self._get_data()
+        if self._validation_check is None or self._validation_check(data):
+            if self._callback_args is not None:
+                self._callback(data, self._callback_args)
+            else:
+                self._callback(data)
+            self._cancel()
+
+    def _cancel(self):
+        """
+        Close the whole dialog.
+        """
+        self.destroy()
--- chadlib/gui/text_entry_dialog.py
+++ chadlib/gui/text_entry_dialog.py
@@ -1,57 +1,27 @@
-from tkinter import Button, Entry, Toplevel
+from tkinter    import Entry
+
+from .dialog    import Dialog
 
 
-class TextEntryDialog(Toplevel):
+class TextEntryDialog(Dialog):
     """
     Single entry textbox dialog.
-
-    Accepts a dialog title, callback to invoke upon confirmation, data to 
-    pass along to the callback to allow for capturing state, and an optional 
-    validation method to ensure the data from the dialog meets some conditions.
     """
 
-    def __init__(self, text_prompt, callback, callback_args, 
+    def __init__(self, text_prompt, callback, callback_args = None, 
                     validation_check = None):
-        super().__init__()
+        super().__init__(text_prompt, callback, callback_args, 
+                            validation_check)
 
-        self.title(text_prompt)
-        self.callback = callback
-        self.callback_args = callback_args
-        self.validation_check = validation_check
-
+    def _create_widgets(self):
+        super()._create_widgets()
         self.text_entry = Entry(self)
-        self.confirm_button = Button(self, text = "Confirm", 
-                                        command = self._confirm)
-        self.cancel_button = Button(self, text = "Cancel", 
-                                    command = self._cancel)
 
+    def _arrange_widgets(self):
         self.text_entry.grid(row = 0, column = 0, columnspan = 2)
         self.text_entry.focus()
         self.confirm_button.grid(row = 1, column = 1)
         self.cancel_button.grid(row = 1, column = 0)
 
-        self.bind("<Return>", lambda event: self._confirm())
-        self.bind("<Escape>", lambda event: self._cancel())
-
-    def _confirm(self):
-        """
-        Call the given callback, pass in the entered text and the given data, 
-        then close the dialog.
-
-        If a validation check was given, ensure it passed before passing the 
-        data back.
-        TODO CJR:  Feedback if the validation check fails.
-        """
-        text = self.text_entry.get()
-        if self.validation_check is None or self.validation_check(text):
-            if self.callback_args is not None:
-                self.callback(text, self.callback_args)
-            else:
-                self.callback(text)
-            self._cancel()
-
-    def _cancel(self):
-        """
-        Close the dialog without taking any action.
-        """
-        self.destroy()
+    def _get_data(self):
+        return self.text_entry.get()
