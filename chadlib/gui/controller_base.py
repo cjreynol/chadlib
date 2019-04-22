12b62b3f70bf95483fb6f979239ed7f04e98de1e
Chad Reynolds <cjreynol13@aol.com>
2019-04-21 23:19:19 -0500

    Add support for saving and loading
    If the proper class attributes are defined, and the proper methods are
    overridden, then save and load support is automatically added to the
    menu.

--- chadlib/gui/controller_base.py
+++ chadlib/gui/controller_base.py
@@ -1,7 +1,11 @@
-from abc            import ABC, abstractmethod
+from abc                import ABC, abstractmethod
+from os                 import makedirs
+from os.path            import exists, expanduser, join
+from platform           import system
+from tkinter.filedialog import askopenfilename, asksaveasfilename
 
-from .menu_setup    import MenuSetup
-from .root_window   import RootWindow
+from .menu_setup        import MenuSetup
+from .root_window       import RootWindow
 
 
 class ControllerBase(ABC):
@@ -10,10 +14,24 @@ class ControllerBase(ABC):
     RootWindow class.
     """
 
-    def __init__(self, title, application_state = None, default_view = None, 
+    APPLICATION_NAME = None
+    FILE_EXTENSION = None   # .something
+    FILETYPES = None        # (("application files", "*" + FILE_EXTENSION),)
+
+    SUPPORT_DIRS = {"Darwin" : expanduser(join("~", "Library", 
+                                            "Application Support")),
+                    "Windows" : expanduser(join("~", "AppData", "Local")) }
+
+    def __init__(self, application_state = None, default_view = None, 
                     window_class = RootWindow):
+        if self.APPLICATION_NAME is None:
+            raise RuntimeError("Must define APPLICATION NAME.")
+
+        self.SUPPORT_DIR = join(self.SUPPORT_DIRS[system()], 
+                                self.APPLICATION_NAME)
         self.window = window_class(self, application_state, default_view, 
-                                    title, self.get_menu_data())
+                                    self.APPLICATION_NAME, 
+                                    self.get_menu_data())
         self.application_state = application_state
 
     @property
@@ -33,8 +51,60 @@ class ControllerBase(ABC):
         """
         Create the initial menu setup with default actions.
 
-        Expected to be extended by subclasses, not overriden.
+        Expected to be extended by subclasses, not overridden.
         """
         menu_setup = MenuSetup()
-        menu_setup.add_submenu_item("Window", "Close Application", self.stop, "{}-w")
+        if self.FILE_EXTENSION is not None and self.FILETYPES is not None:
+            menu_setup.add_submenu_item("File", "Save Drawings", self.save, 
+                                        "{}-s")
+            menu_setup.add_submenu_item("File", "Load Drawings", self.open, 
+                                        "{}-o")
+
+        menu_setup.add_submenu_item("Window", "Close Application", self.stop, 
+                                    "{}-w")
         return menu_setup
+
+    def ensure_support_dir_exists(self):
+        """
+        Check if the support directory exists, if not then create it.
+        """
+        if not exists(self.SUPPORT_DIR):
+            makedirs(self.SUPPORT_DIR)
+
+    def open(self):
+        """
+        Prompt the user for a filename and open/load that file.
+        """
+        self.ensure_support_dir_exists()
+        filename = askopenfilename(initialdir = self.SUPPORT_DIR, 
+                                    filetypes = self.FILETYPES) 
+        if filename is not None:
+            self._open_logic(filename)
+
+    def save(self):
+        """
+        Prompt the user for a file and write data to that file.
+        """
+        self.ensure_support_dir_exists()
+        filename = asksaveasfilename(initialdir = self.SUPPORT_DIR, 
+                                        defaultextension = 
+                                            self.FILE_EXTENSION,
+                                        filetypes = self.FILETYPES) 
+        if filename is not None:
+            self._save_logic(filename)
+
+    # the following two methods only need to be overridden if saving and 
+    # loading is implemented
+    def _open_logic(self, filename):
+        """
+        Placeholder to be overridden by subclasses, intended to take load 
+        data from the chosen filename.
+        """
+        pass
+
+    def _save_logic(self, filename):
+        """
+        Placeholder to be overriden by subclasses, intended to get data from 
+        the applicaiton and write it to the given file.
+        """
+        pass
